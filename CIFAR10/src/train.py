2f36d3a723551e6e72d223da85a55d6638924e9e
AntonioFerreras <antfs10@gmail.com>
2024-12-24 01:01:12 -0500

    Grad scaling to prevent nans from amp

--- CIFAR10/src/train.py
+++ CIFAR10/src/train.py
@@ -61,8 +61,8 @@ if __name__ == '__main__':
     test_dataset = datasets.CIFAR10(root="../data", train=False, transform=data_augmentation.get_test_transforms(), download=True)
 
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2, persistent_workers=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2, persistent_workers=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"CUDA is available: {torch.cuda.is_available()}")
@@ -86,18 +86,19 @@ if __name__ == '__main__':
     if args.profile:
         writer = SummaryWriter(log_dir="./logs")
 
+    scaler = torch.cuda.amp.GradScaler()
+
     for epoch in range(epochs):
         start_time = time.time()
-        # Training phase
         model.train()
         running_loss = torch.tensor(0.0, device=device)
         correct_train = torch.tensor(0, device=device, dtype=torch.int64)
         total_train = torch.tensor(0, device=device, dtype=torch.int64)
 
-        do_profile = args.profile and epoch == 1
+        do_profile = args.profile and epoch >= 1
         prof = None
         if do_profile:
-            # Profiler Setup
+            # Profiler setup
             prof = profile(
                 activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
                 on_trace_ready=torch.profiler.tensorboard_trace_handler('./logs'),
@@ -109,37 +110,30 @@ if __name__ == '__main__':
 
         for batch_idx, (images, labels) in enumerate(train_loader):
             with (record_function("data transfer") if do_profile else DummyContextManager()):
-                images, labels = images.to(device=device, non_blocking=True), labels.to(device=device, non_blocking=True)
+                images, labels = images.to(device, non_blocking=True), labels.to(device, non_blocking=True)
 
-                # Do normalization in training loop to reduce CPU-GPU transfer time
-                #    - This is not necessary if the normalization is done in the data augmentation transforms
-                #    - Normalizing in data aug transforms converts to FP32, which is slower to transfer than the
-                #      original 8 bit images
-                # images = (images.to(torch.float32) / 255.0 - data_augmentation.MEAN) / data_augmentation.STD
+            optimizer.zero_grad()
 
-            # Save first 24 images of the first batch of the first epoch and apply unnormalization
-            # if first_epoch and batch_idx == 0:
-            #     unnormalized_images = unnormalize_batch(images[:24].cpu(), mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
-            #     save_image(unnormalized_images, 'first_24_images.png', nrow=6)
-            #     first_epoch = False
-
-            # Forward pass
             with torch.autocast(device_type='cuda', dtype=torch.float16):
                 with (record_function("model forward") if do_profile else DummyContextManager()):
                     outputs = model(images)
+
                 with (record_function("loss computation") if do_profile else DummyContextManager()):
                     loss = criterion(outputs, labels)
 
-            # Backward pass
-            optimizer.zero_grad()
             with (record_function("backward pass") if do_profile else DummyContextManager()):
-                loss.backward()
+                # Backward pass with gradient scaling
+                scaler.scale(loss).backward()
 
-            if grad_clip: 
-                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+            if grad_clip:
+                with (record_function("gradient clipping") if do_profile else DummyContextManager()):
+                    scaler.unscale_(optimizer)  # Unscale gradients before clipping
+                    nn.utils.clip_grad_value_(model.parameters(), grad_clip)
 
             with (record_function("optimizer step") if do_profile else DummyContextManager()):
-                optimizer.step()
+                scaler.step(optimizer)
+
+            scaler.update()
                 
             with (record_function("metrics calculation") if do_profile else DummyContextManager()):
                 running_loss += loss.detach() * images.size(0)
@@ -188,7 +182,7 @@ if __name__ == '__main__':
                 correct_test += predicted.eq(labels).sum().item()
 
         test_accuracy = 100.0 * correct_test / total_test
-
+        # test_accuracy = 0.0
         # Log validation metrics
         if writer:
             writer.add_scalar("Accuracy/Test", test_accuracy, epoch)
