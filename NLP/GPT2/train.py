8ae9d6f635355b31460d9e73fe5eadf8b1a30aa2
AntonioFerreras <antfs10@gmail.com>
2025-01-08 19:27:02 -0500

    Computational Optimizations

--- NLP/GPT2/train.py
+++ NLP/GPT2/train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import model
 from data import get_shakespeare_dataloader
+import time
 
 num_return_sequences = 5
 max_length = 30
@@ -12,35 +13,42 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed(42)
 
 # autodetect device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device_name = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(device_name)
 print("using device: %s" % device)
 
 # load dataset and dataloader
-batch_size = 128
-seq_length = 32
+batch_size = 16
+seq_length = 1024
 shakespeare_text = open('tiny_shakespeare.txt').read()
 dataloader = get_shakespeare_dataloader(batch_size, seq_length, shakespeare_text)
 
-# calculate number of batches in an epoch
-num_batches = len(dataloader)
-print("Number of batches in an epoch: %d" % num_batches)
+torch.set_float32_matmul_precision('high')
 
 # load the model
-# llm = model.GPT.from_pretrained('gpt2')
-llm = model.GPT(model.GPTConfig())
+llm = model.GPT(model.GPTConfig(vocab_size=50304))
 llm.to(device)
+llm = torch.compile(llm)
 
 # train
 optimizer = torch.optim.AdamW(llm.parameters(), lr=3e-4)
 for batch_idx, (input_seq, target_seq) in enumerate(dataloader):
+    t0 = time.time()
+
     input_seq, target_seq = input_seq.to(device), target_seq.to(device)
     
     optimizer.zero_grad()
-    logits, loss = llm(input_seq, target_seq)
-    print("batch %d, loss %.3f" % (batch_idx, loss.item()))
-
+    with torch.autocast(device_type=device_name, dtype=torch.bfloat16):
+        logits, loss = llm(input_seq, target_seq)
     loss.backward()
     optimizer.step()
+
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0)*1000 # ms
+    tokens_per_sec = batch_size * seq_length / (t1 - t0)
+    print("batch %d, loss %.3f, time %.2f ms, tok/sec %.2f" % (batch_idx, loss.item(), dt, tokens_per_sec))
 
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
