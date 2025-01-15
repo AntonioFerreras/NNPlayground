Gradient accumulation, fix tokenization in training

NLP/GPT2/data.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import tiktoken
 
 class ShakespeareDataset(Dataset):
     def __init__(self, text, seq_length):
@@ -11,10 +12,8 @@ class ShakespeareDataset(Dataset):
         """
         self.seq_length = seq_length
         self.text = text
-        self.chars = sorted(set(text))  # Unique characters
-        self.char_to_idx = {ch: i for i, ch in enumerate(self.chars)}
-        self.idx_to_char = {i: ch for i, ch in enumerate(self.chars)}
-        self.encoded_text = [self.char_to_idx[ch] for ch in text]  # Convert text to integers
+        enc = tiktoken.get_encoding('gpt2')
+        self.encoded_text = enc.encode(text)  # Convert text to integers
 
     def __len__(self):
         """Returns the number of sequences available."""
@@ -25,6 +24,10 @@ class ShakespeareDataset(Dataset):
         input_seq = self.encoded_text[idx:idx + self.seq_length]
         target_seq = self.encoded_text[idx + 1:idx + self.seq_length + 1]
         return torch.tensor(input_seq, dtype=torch.long), torch.tensor(target_seq, dtype=torch.long)
+
+    def num_tokens(self):
+        """Returns the number of tokens in the dataset."""
+        return len(self.encoded_text)
 
 def get_shakespeare_dataloader(batch_size, seq_length, text, shuffle=True):
     """
@@ -40,5 +43,7 @@ def get_shakespeare_dataloader(batch_size, seq_length, text, shuffle=True):
         DataLoader: PyTorch DataLoader instance for the dataset.
     """
     dataset = ShakespeareDataset(text, seq_length)
+    num_tokens = dataset.num_tokens()
+    print(f"Number of tokens in the dataset: {num_tokens}")
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
-    return dataloader
+    return dataloader, num_tokens

NLP/GPT2/train.py
@@ -6,8 +6,7 @@ import model
 from data import get_shakespeare_dataloader
 import time
 
-num_return_sequences = 5
-max_length = 30
+
 
 torch.manual_seed(42)
 if torch.cuda.is_available():
@@ -19,13 +18,15 @@ device = torch.device(device_name)
 print("using device: %s" % device)
 
 # load dataset and dataloader
-batch_size = 524288
-mini_batch_size = 16
-seq_length = 1024
+batch_size = 2 ** 18 # 524288 # in tokens
+mini_batch_size = 16 # in batch dimension
+seq_length = 1024 # in tokens
 assert batch_size % (mini_batch_size * seq_length) == 0, "batch size must be divisible by mini batch size"
 grad_accum_steps = batch_size // (mini_batch_size * seq_length)
 shakespeare_text = open('tiny_shakespeare.txt').read()
-dataloader = get_shakespeare_dataloader(batch_size, seq_length, shakespeare_text)
+dataloader, num_tokens = get_shakespeare_dataloader(mini_batch_size, seq_length, shakespeare_text)
+num_batches_in_epoch = num_tokens // batch_size
+print("num batches in epoch: %d" % num_batches_in_epoch)
 
 torch.set_float32_matmul_precision('high')
 
@@ -34,7 +35,7 @@ llm = model.GPT(model.GPTConfig(vocab_size=50304))
 llm.to(device)
 llm = torch.compile(llm)
 
-max_lr = 6e-4
+max_lr = 6e-4 * 2
 min_lr = max_lr * 0.1
 warmup_steps = 10
 max_steps = 50 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
@@ -51,6 +52,8 @@ def get_lr(it):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
 
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
 
 # train
 optimizer = llm.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device_type=device_name)
@@ -59,13 +62,17 @@ lr = 0.0
 for batch_idx, (input_seq, target_seq) in enumerate(dataloader):
     t0 = time.time()
 
-    input_seq, target_seq = input_seq.to(device), target_seq.to(device)
-    
     # forward then backward
     optimizer.zero_grad()
-    with torch.autocast(device_type=device_name, dtype=torch.bfloat16):
-        logits, loss = llm(input_seq, target_seq)
-    loss.backward()
+    loss_sum = 0.0
+    for mini_step in range(grad_accum_steps):
+        input_seq, target_seq = input_seq.to(device), target_seq.to(device)
+        with torch.autocast(device_type=device_name, dtype=torch.bfloat16):
+            logits, loss = llm(input_seq, target_seq)
+        loss = loss / grad_accum_steps
+        loss_sum += loss.detach()
+        loss.backward()
+    
     grad_norm = nn.utils.clip_grad_norm_(llm.parameters(), max_norm=1.0)
 
     # step the scheduler
@@ -79,14 +86,31 @@ for batch_idx, (input_seq, target_seq) in enumerate(dataloader):
         torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1 - t0)*1000 # ms
-    tokens_per_sec = batch_size * seq_length / (t1 - t0)
-    print("batch %d, loss %.3f, lr %.2e, grad_norm %.4f, time %.2f ms, tok/sec %.2f" % (batch_idx, loss.item(), lr, grad_norm, dt, tokens_per_sec))
+    tokens_per_sec = batch_size / (t1 - t0)
+    print("batch %d, loss %.3f, lr %.2e, grad_norm %.4f, time %.2f ms, tok/sec %.2f" % (batch_idx, loss_sum.item(), lr, grad_norm, dt, tokens_per_sec))
+    
+    # print some generated text
+    if step % 10 == 0:
+        num_return_sequences = 5
+        max_length = 30
+        tokens = enc.encode("O Romeo, Romeo! ")
+        tokens = torch.tensor(tokens, dtype=torch.long)
+        tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1).to(device)
+        while tokens.size(1) < max_length:
+            with torch.no_grad():
+                logits, _ = llm(tokens)
+                next_token_logits = logits[:, -1, :]
+                next_token_probs = F.softmax(next_token_logits, dim=-1)
+                topk_probs, topk_indices = torch.topk(next_token_probs, 50, dim=-1)
+                idx = torch.multinomial(topk_probs, 1)
+                xcol = torch.gather(topk_indices, -1, idx)
+                tokens = torch.cat((tokens, xcol), dim=1)
+        for i in range(num_return_sequences):
+            out_tokens = tokens[i, :max_length].tolist()
+            print('>', enc.decode(out_tokens))
+        if step % 50 == 0:
+            torch.save(llm.state_dict(), f"weights/model_step_{step}.pt")
 
     step += 1
 
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long) # (T,)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (B, T)
-x = tokens.to(device)
+